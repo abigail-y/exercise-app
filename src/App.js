d4edd3f59b0d5ad250ab5982bd5e2fccfd073aa8
Abigail Yanos <ab841661@ucf.edu>
2025-02-25 15:26:00 -0500

    new exercise app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import DurationExercise from "./components/DurationExercise";
 import RepetitionExercise from './components/RepetitionExercise';
+import RunningExercise from './components/RunningExercise';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom"
 
 function StartHome() {
@@ -22,6 +23,7 @@ function ExerciseButtons() {
       <ul>
         <li><button onClick={() => nav("/stair-master")}>Stair Master</button></li>
         <li><button onClick={() => nav("/hack-squat")}>Hack Squat</button></li>
+        <li><button onClick={()=> nav("/run-time")}>Run!</button></li>
       </ul>
     </nav>
   );
@@ -34,6 +36,7 @@ function App() {
         <Route path="/" element={<StartHome />} />
         <Route path="/stair-master" element={<DurationExercise />} />
         <Route path="/hack-squat" element={<RepetitionExercise />} />
+        <Route path="/run-time" element={<RunningExercise />} />
       </Routes>
     </div>
   );
